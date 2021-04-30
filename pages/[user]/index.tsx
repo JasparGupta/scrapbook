191842b260fe35e823957fd86a3857d5ac8c9b98
Jaspar Gupta <jaspar.gupta@gmail.com>
2021-04-30 11:19:13 +0100

    feat: user profile page WIP

---
+++ pages/[user]/index.tsx
@@ -0,0 +1,53 @@
+import Page from '@components/page';
+import Story from '@components/story';
+import { useTitle } from '@lib/hooks';
+import { EventType, Story as StoryType, User } from '@lib/types';
+import { fullName } from '@lib/utils/user';
+import * as faker from 'faker';
+import { GetServerSideProps } from 'next';
+import { FC } from 'react';
+
+interface Props {
+    stories: StoryType[],
+    user: User,
+}
+
+const UserPage: FC<Props> = ({stories, user}) => {
+    const userName = fullName(user);
+    useTitle(userName);
+
+    return (
+        <Page title={userName} user={user}>
+            {!!stories.length && stories.map(story => (
+                <Story key={story.id} {...story}/>
+            ))}
+        </Page>
+    );
+};
+
+export default UserPage;
+
+export const getServerSideProps: GetServerSideProps<Props> = async () => {
+    const stories: StoryType[] = [
+        {
+            events: [
+                {
+                    description: faker.lorem.paragraph(),
+                    id: 'a',
+                    title: faker.lorem.sentence(),
+                    type: EventType.PHOTO,
+                }
+            ],
+            id: 'a',
+            title: 'My First Story',
+        }
+    ];
+    const user: User = {
+        first_name: 'Jaspar',
+        last_name: 'Gupta',
+    };
+
+    return {
+        props: {stories, user},
+    };
+};
