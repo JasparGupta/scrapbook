429980891e6d3a6b01f2762a51eddceab6b21b1a
Jaspar Gupta <jaspar.gupta@gmail.com>
2021-04-30 11:18:41 +0100

    feat: story component WIP

---
+++ components/story/index.tsx
@@ -0,0 +1,30 @@
+import Event from '@components/event';
+import { Event as EventType } from '@lib/types';
+import { FC } from 'react';
+import Card from 'react-bootstrap/Card';
+
+interface Props {
+    events: EventType[],
+    id: string,
+    title: string,
+}
+
+const Story: FC<Props> = ({events, title}) => {
+    return (
+        <Card>
+            <Card.Header>
+                <p className="lead mb-0">{title}</p>
+            </Card.Header>
+
+            <Card.Body>
+                {events.map(event => <Event key={event.id} {...event}/>)}
+            </Card.Body>
+
+            <Card.Footer>
+
+            </Card.Footer>
+        </Card>
+    );
+};
+
+export default Story;
