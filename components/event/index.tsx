c9a72ca3a8034b9e9716350fd097e7cc07a465c5
Jaspar Gupta <jaspar.gupta@gmail.com>
2021-04-30 11:18:30 +0100

    feat: event component WIP

---
+++ components/event/index.tsx
@@ -0,0 +1,30 @@
+import { FC } from 'react';
+import Card from 'react-bootstrap/Card';
+import EventType from '../../lib/types/event-type';
+
+interface Event {
+    description?: string,
+    id: string,
+    title?: string,
+    type: EventType,
+}
+
+const Event: FC<Event> = ({description, title}) => {
+    return (
+        <Card>
+            <Card.Header>
+                {title}
+            </Card.Header>
+
+            <Card.Body>
+                <p>{description}</p>
+            </Card.Body>
+
+            <Card.Footer>
+
+            </Card.Footer>
+        </Card>
+    );
+}
+
+export default Event;
