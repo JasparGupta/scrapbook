9f323efcf914e1dce9fbaccdfde1675fa6132a6e
Jaspar Gupta <jaspar.gupta@gmail.com>
2021-05-01 20:10:53 +0100

    feat: return type for user hook

--- lib/hooks/user.ts
+++ lib/hooks/user.ts
@@ -1,6 +1,7 @@
 import { UserContext } from '@lib/contexts';
+import { User } from '@lib/types';
 import { useContext } from 'react';
 
-export default function useUser() {
+export default function useUser(): User {
     return useContext(UserContext);
 }
