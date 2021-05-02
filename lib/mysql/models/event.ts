616da5f6b1abf09da0d069c072bf176d750e1d77
Jaspar Gupta <jaspar.gupta@gmail.com>
2021-05-02 14:41:35 +0100

    feat: event model

---
+++ lib/mysql/models/event.ts
@@ -0,0 +1,15 @@
+import sequelize from '@lib/mysql';
+import { DataTypes } from 'sequelize';
+
+const Event = sequelize.define('Event', {
+    description: DataTypes.TEXT,
+    title: DataTypes.STRING,
+    type: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+    },
+});
+
+Event.sync({force: process.env.NODE_ENV === 'development'});
+
+export default Event;
